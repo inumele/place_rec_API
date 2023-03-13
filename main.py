665d61ca2ce158413d1641639b00f5a9319f7ec3
inumele <insaf.g2001@gmail.com>
2023-03-13 17:37:22 +0300

    Commit

--- main.py
+++ main.py
@@ -5,10 +5,20 @@ person_ratings = [
     (0, 4),
     (23, 3),
     (25, 5),
-    (156, 4),
-    (356, 5)
+    (156, 1),
+    (356, 1)
 ]
 
-res = RecommendationEngine('people_ratings.csv', 'places.csv').get_places(person_ratings, [55.823713, 49.059503])
+chosen_categories = [
+    'кафе',
+    'развлечения',
+    'музей'
+]
+
+res = RecommendationEngine('people_ratings.csv', 'places.csv').get_places(
+    person_ratings,
+    [55.773554, 49.184842],
+    chosen_categories
+)
 print(res.head(50))
 
